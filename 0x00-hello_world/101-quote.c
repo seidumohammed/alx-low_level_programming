305f9cabef3062b5ace3019b26edde4fc7cbfb58
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-08 18:25:01 +0000

    101-quote.c fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
+
 
 /**
- * main = entry point
+ * main - Entry point
  *
- * return: prints 1 for  (Error)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+
+return (1);
 }
+
