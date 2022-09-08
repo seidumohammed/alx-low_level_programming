6e5f870ed43cda14565fa30d9fe6608afcb48840
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-08 17:50:14 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *  Program entry point is main
+ *
+ *  Return prints an integer of zero(successfully run program)
+ */
+
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+	return (0);
+
+}
