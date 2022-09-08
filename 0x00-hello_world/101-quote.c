6996195a51fbf01eac09649b8807edc0f402c5fd
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-08 17:54:08 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main = entry point
+ *
+ * return: prints 1 for  (Error)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
