0ccf9585269dbdd35b4eab87c5307685eb4b8e47
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-10 03:41:03 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+{
+	int i;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (k = 48; k < 58; k++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(k);
+				if (k == 57 && i == 56)
+				{
+					break
+						;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
