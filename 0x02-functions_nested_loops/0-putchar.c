c225e6b6c6e6b3a1bbb553abd37a79673df303d2
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-13 16:38:09 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+ * main - print putchar
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
