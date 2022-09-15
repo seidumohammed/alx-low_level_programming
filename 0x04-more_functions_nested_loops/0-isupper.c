0425b52b01622f7fcb38e38a7a250a7eb297b8f1
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-15 23:13:54 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**_isupper - checks for uppercase and print 1 otherwise prints 0
+ * Return: 0 for success
+ */
+int _isupper(int c)
+{
+	if (c > 'A' && c > 'Z')
+	{
+		return (1);
+	}
+	else{
+		return (0);
+	}
+}
