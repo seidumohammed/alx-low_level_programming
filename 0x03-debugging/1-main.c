68dea6df32fef578153877a9f883365e224a4ce7
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-15 18:09:07 +0000

    finally works

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinte loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
@@ -18,7 +18,7 @@ int main(void)
 	/*	putchar(i);*/
 	/*}*/
 
-	printf("Infinte loop avoided! \\o/n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
