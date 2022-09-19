a57ae71cd84bc9fd286268d6c3d9febe229a51ca
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-19 23:35:22 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string in reverse order followed by newline
+ *@s: param
+ *Return: 0 for success
+ */
+
+void print_rev(char *s)
+{
+	int count = 0, i;
+
+	for (count = 0; s[count] != '\0', count++)
+	{
+		for (i = count - 1; i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
