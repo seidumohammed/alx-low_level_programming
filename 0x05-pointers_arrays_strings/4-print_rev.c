2b8a0b8f0816b006275272117f28c36d5cb83316
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-20 00:55:55 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,14 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-	for (; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		for (j = i - 1; j >= 0; j--)
-		{
-			_putchar(s[j]);
-		}
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
