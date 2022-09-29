9b8a64d0b9850fc0cd1b25130de7c4af07b40ee6
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-29 04:16:03 +0000

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - compares two strings, returns 1 if identical and 0 otherwise
+ * @s1: input
+ * @s2: input
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
