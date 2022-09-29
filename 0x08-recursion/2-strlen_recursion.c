623249324d7b496e636331e6e07833887b434852
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-29 03:58:01 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: input
+ * Return: 0 for Success
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
