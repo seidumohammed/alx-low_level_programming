8f9cca62541d83541bf1fc075541c89abb24620c
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-29 04:08:13 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int squareroot(int n, int i);
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: input
+ * Return: 0 for success
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
+/**
+ * squareroot - checks for perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int squareroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
+}
