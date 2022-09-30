95d0d1af5d52eec14b6e3b3f07b98cbe59193acd
Seidu Mohammed <seidumohammed64@gmail.com>
2022-09-30 23:19:24 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-
-	if (argc < 1)
-		return (0);
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
